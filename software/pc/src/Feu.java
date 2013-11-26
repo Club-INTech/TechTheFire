99981204c2ea42386c41520f6281ef02553b3af9
Dede <dede@plopmail.fr>
2013-11-26 17:37:59 +0100

    Début de remise au propre des feux

--- software/pc/src/Feu.java
+++ software/pc/src/Feu.java
@@ -1,10 +1,12 @@
 
 public class Feu extends Elements_de_jeu {
-	protected enum Colour {RED, YELLOW, UNDETERMINED} ;	
-	protected enum Orientation {GROUND, XPLUS, YPLUS, UNDETERMINED}; //XPLUS = orienté de couleur couleur vers les X croissants, suppose d'être debout
-																	//De même pour YPLUS. GROUND signifie que le feu est à l'horizontal
-	public Feu()
+	public Feu(int id, int height, Orientation orientation, Colour colour, Position position)
 	{
+		this.id = id;
+		this.height = height;
+		this.orientation = orientation;
+		this.colour = colour;
+		this.position = position;
 	}
 	
 	int id ;
@@ -13,122 +15,15 @@ public class Feu extends Elements_de_jeu {
 	Colour colour = Colour.UNDETERMINED ;
 	boolean onFireplace = false ;	// what is ?
 	boolean taken  = false ;	// IDEM
-	Vec2 position ;
 	
 
-	// TODO : Comment this ID to explain what is the matter of this function.
-	// Like the param a, what is it, and build a documentation of which ID coressponds to what.
-	public void define_id(int a)	//Cette méthode appelée pour chaque feu fixe ses paramètres initiaux
-	{
-		this.id = a;
-		if ( a==4 || a==5 || a==6 )
-		{
-			this.orientation = Orientation.GROUND ;
-			this.height = (a-3);
-			this.position.x = 1100;
-			this.position.y = 2100;
-		}
-		
-		if ( a==11 || a==12 || a==13)
-		{
-			this.orientation = Orientation.GROUND ;
-			this.height = (a-10);
-			this.position.x = 1100;
-			this.position.y = 900;
-		}
-		
-		if (a==1 || a==2 || a==15 ||a==16)
-		{
-			this.orientation = Orientation.YPLUS ;
-			this.colour = Colour.YELLOW ;
-		}
-		
-		switch (a)
-		{
-		case (1) :
-			this.position.x = 600;
-			this.position.y = 2100;
-		break;
-		case (2) :
-			this.position.x = 800;
-			this.position.y = 3000;
-		break;
-		case(3) :
-			this.orientation = Orientation.XPLUS;
-			this.colour = Colour.RED ;
-			this.position.x = 1100;
-			this.position.y = 2600;
-		break;
-		case(4) :
-			this.colour = Colour.YELLOW;
-		break;
-		case(5) :
-			this.colour = Colour.RED ;
-		break;
-		case(6) :
-			this.colour = Colour.YELLOW;
-		break;
-		case(7) :
-			this.orientation = Orientation.YPLUS;
-			this.colour = Colour.RED;
-			this.position.x = 1600;
-			this.position.y = 2100;
-		break;
-		case(8) :
-			this.orientation = Orientation.XPLUS;
-			this.colour = Colour.RED;
-			this.position.x = 2000;
-			this.position.y = 1700;
-		break;
-		case(9) :
-			this.orientation= Orientation.XPLUS;
-			this.colour= Colour.YELLOW;
-			this.position.x = 2000;
-			this.position.y = 1300;
-		break;
-		case(10) :
-			this.orientation=Orientation.YPLUS;
-			this.colour= Colour.RED;
-			this.position.x = 1600;
-			this.position.y = 900;
-		break;
-		case(11) :
-			this.colour=Colour.RED;
-			break;
-		case(12) :
-			this.colour=Colour.YELLOW;
-		break;
-		case(13) :
-			this.colour = Colour.RED;
-		break;
-		case(14) :
-			this.orientation = Orientation.XPLUS;
-			this.colour = Colour.YELLOW;
-			this.position.x = 1100;
-			this.position.y = 400;
-		break;
-		case(15) :
-			this.position.x = 800;
-			this.position.y = 0;
-		break;
-		case(16) :
-			this.position.x = 600;
-			this.position.y = 900;
-		break;
-
-		default :
-				 ;
-		}
-	}	
 	public void pickFire()
 	{
 		this.taken = true;
-		nbStoredFires ++;
 	}
 	
 	public void ejectFire()
 	{
 		this.onFireplace = true;
-		nbStoredFires --;
 	}
 }
