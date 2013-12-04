edee9f718df2088d044177e75a10b5b4ff9064d5
kayou <pierre.bourre@telecom-sudparis.eu>
2013-12-05 00:08:39 +0100

    On ne commit pas les merges!!!!!

--- software/pc/src/lanceur.java
+++ software/pc/src/lanceur.java
@@ -1,25 +1,10 @@
-
-<<<<<<< HEAD
-public class lanceur 
-{
-
-	public static void main(String[] args) 
-	{
-		Container container = Container.initialiser();
-		Log log = (Log)container.get_service("Log");
 		
-		log.debug("Test");
-		log.warning("Test");
-		log.critical("Test");
-		
-=======
 public class lanceur
 {
 	private static Test test = new Test();
 	public static void main(String[] args)
 	{
 		test.test();
->>>>>>> 213f1b59b2080b1f91e33781ab2a4c9d9d820955
 	}
 
 }
