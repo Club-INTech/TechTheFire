afd56cf2113f25ccefa2a5d6eacea90d30b5ec93
Dede <dede@plopmail.fr>
2013-12-05 13:03:56 +0100

    Ajout des bacs et de la fresque

---
+++ software/pc/src/table/Fruit_Tub.java
@@ -0,0 +1,17 @@
+package table;
+
+import smartMath.Vec2;
+
+public class Fruit_Tub extends Game_Element {
+	private float length = 700;
+	private float depth = 300;
+	// les bacs étant rectangulaires, leur position sera celle de leur coin inférieur droit
+	
+	public Fruit_Tub(Vec2 position) 
+	{
+		super(position);
+	}
+	
+	
+	
+}
