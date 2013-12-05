18a6045ab29c6dec0906a8f3a0992dd9147c0ae7
kayou <pierre.bourre@telecom-sudparis.eu>
2013-12-05 20:57:15 +0100

    Le code du capteur de couleur qui fonctionne mieux (j'ai augmenté le prescaler)

--- software/avr/atmega328p/test_capteur_couleurs/main.cpp
+++ software/avr/atmega328p/test_capteur_couleurs/main.cpp
@@ -12,73 +12,80 @@ uint16_t counter = 0, countR = 0, countG = 0, countB = 0;
 void interruption_timer();
 void interruption_int0();
 
-int main() {
+int main()
+{
 
-  D6::output();
-  D5::output();
-  D4::output();
-  D3::output();
-  D2::input();
+	D6::output();
+	D5::output();
+	D4::output();
+	D3::output();
+	D2::input();
 
-  uart0::init();
-  uart0::change_baudrate(9600); // vitesse de connection: 9600
+	uart0::init();
+	uart0::change_baudrate(9600); // vitesse de connection: 9600
 
-  D3::high();
-  D4::high();
-  D5::low();
-  D6::low();
+	D3::high();
+	D4::high();
+	D5::low();
+	D6::low();
 
-  timer0::mode(timer0::MODE_COUNTER);
-  timer0::prescaler(timer0::PRESCALER_1);
+	timer0::mode(timer0::MODE_COUNTER);
+	timer0::prescaler(timer0::PRESCALER_128);
 
-  timer0::counter::overflow_interrupt::attach(interruption_timer);
-  timer0::counter::overflow_interrupt::enable();
+	timer0::counter::overflow_interrupt::attach(interruption_timer);
+	timer0::counter::overflow_interrupt::enable();
 
-  int0::mode(int0::LOW_LEVEL);
-  int0::attach(interruption_int0);
-  int0::enable();
+	int0::mode(int0::LOW_LEVEL);
+	int0::attach(interruption_int0);
+	int0::enable();
 
-  char buffer[17];  
+	char buffer[17];  
 
-  while(1){
-    uart0::read(buffer);
-    if(!strcmp(buffer,"?")){
-
-      uart0::printfln("red = %d, blue = %d, green = %d", countR, countB, countG);
-    }
-  }
+	while(1)
+	{
+		uart0::read(buffer);
+		if(!strcmp(buffer,"?"))
+		{
+			uart0::printfln("red = %d, blue = %d, green = %d", countR, countB, countG);
+		}
+	}
 }
 
-void interruption_timer(){
+void interruption_timer()
+{
 
-  flag++;
-  if(flag == 1){
-    countR = counter;
-    D5::high();
-    D6::high();
+	flag++;
+	if(flag == 1)
+	{
+		countR = counter;
+		D5::high();
+		D6::high();
 
-   }
-  else if(flag == 2){
+	}
+	else if(flag == 2)
+	{
 
-    countG = counter;
-    D5::low();
-    D6::high();
-  
-  }
-  else if(flag == 3){
+		countG = counter;
+		D5::low();
+		D6::high();
 
-    countB = counter;
-    D5::low();
-    D6::low();
-  }
-  else if(flag == 4){
+	}
+	else if(flag == 3)
+	{
 
-    flag = 0;
-  }
-  counter = 0;
+		countB = counter;
+		D5::low();
+		D6::low();
+	}
+	else if(flag == 4)
+	{
+
+		flag = 0;
+	}
+	counter = 0;
 }
 
-void interruption_int0(){
-
-  counter++;
+void interruption_int0()
+{
+	counter++;
 }
