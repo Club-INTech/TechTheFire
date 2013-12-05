ca7110f467dc91c1dc5ddbff95840001ad28d1bd
Dede <dede@plopmail.fr>
2013-12-05 20:01:38 +0100

    Modif fruit

--- software/pc/src/table/Fruit.java
+++ software/pc/src/table/Fruit.java
@@ -6,6 +6,7 @@ public class Fruit extends Game_Element {
 
 	private int fruit_id; // face à l'arbre, le fruit 0 est celui juste à droite, et on tourne dans le sens trigonométrique
 	private boolean taken = false;
+	private boolean good = true;
 	
 	public Fruit (Vec2 position, int ID)
 	{
