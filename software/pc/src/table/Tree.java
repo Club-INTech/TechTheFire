ac2aaec85ebd2b39682e7f8890104ff4f5f0a082
PF <troll@yopmail.fr>
2013-12-12 15:32:46 +0100

    Suite du précédent commit

--- software/pc/src/table/Tree.java
+++ software/pc/src/table/Tree.java
@@ -11,16 +11,16 @@ public class Tree extends Game_Element {
 	// combien gauche()
 	// combien droit()
 
-	public Tree (Vec2 position, int id, Vec2 pos0, Vec2 pos1, Vec2 pos2, Vec2 pos3, Vec2 pos4, Vec2 pos5)
+	public Tree (Vec2 position, int id)
 	{
 		super(position);
 		this.tree_id = id;
-		this.arrayFruit[0] = new Fruit(pos0, 0);
-		this.arrayFruit[1] = new Fruit(pos1, 1);
-		this.arrayFruit[2] = new Fruit(pos2, 2);
-		this.arrayFruit[3] = new Fruit(pos3, 3);
-		this.arrayFruit[4] = new Fruit(pos4, 4);
-		this.arrayFruit[5] = new Fruit(pos5, 5);	
+		this.arrayFruit[0] = new Fruit(0);
+		this.arrayFruit[1] = new Fruit(1);
+		this.arrayFruit[2] = new Fruit(2);
+		this.arrayFruit[3] = new Fruit(3);
+		this.arrayFruit[4] = new Fruit(4);
+		this.arrayFruit[5] = new Fruit(5);	
 	}
 	
 	public Tree(Vec2 position, int tree_id, float radius, Fruit[] arrayFruit) {
