85f26df228296c67cfab8e508234c50d0d13c9a9
PF <troll@yopmail.fr>
2013-12-12 01:20:30 +0100

    Nombre de feux sur un foyer

--- software/pc/src/table/Fireplace.java
+++ software/pc/src/table/Fireplace.java
@@ -3,12 +3,14 @@ package table;
 import smartMath.Vec2;
 
 public class Fireplace extends Game_Element {
-	private float radius ;
+	private float radius;
+	private int placed_fires;
 	
 	public Fireplace (Vec2 position, float rad)
 	{
 		super(position);
 		this.radius = rad;
+		placed_fires = 0;
 	}
 	
 }
