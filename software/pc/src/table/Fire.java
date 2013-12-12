a74a7d69e360c5c1ad65f3e93618477e032dd68a
PF <troll@yopmail.fr>
2013-12-12 01:17:30 +0100

    Obstacle n'est plus une classe abstraite

--- software/pc/src/table/Fire.java
+++ software/pc/src/table/Fire.java
@@ -6,9 +6,9 @@ import smartMath.Vec2;
 public class Fire extends Game_Element {
 
 	private int fire_id ;//numérotation : par x décroissants et à x égaux par y décroissants (en gros de haut en bas, de gauche à droite dans le règlement)
-	private int height=0 ; //0=sol, chaque unité supplémentaire représente un feu en-dessous	
-	private Orientation orientation = Orientation.UNDETERMINED ;
-	private Colour colour = Colour.UNDETERMINED ;
+	private int height ; //0=sol, chaque unité supplémentaire représente un feu en-dessous	
+	private Orientation orientation;
+	private Colour colour;
 	private boolean onFireplace = false ;	// Booléen qui dit si le feu est sur un foyer ou non
 	private boolean taken  = false ;	// Booléen indiquant si le feu a été pris
 	
