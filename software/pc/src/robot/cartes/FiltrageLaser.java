8ee06c4b3f8a86335fa2a2e7533b562c6e684790
PF <troll@yopmail.fr>
2013-12-13 18:03:13 +0100

    Pas de mat2 (faudra trouver une lib de calcul matriciel)

--- software/pc/src/robot/cartes/FiltrageLaser.java
+++ software/pc/src/robot/cartes/FiltrageLaser.java
@@ -1,6 +1,5 @@
 package robot.cartes;
 
-import smartMath.Mat2;
 import utils.Log;
 import utils.Read_Ini;
 import container.Service;
@@ -16,8 +15,6 @@ public class FiltrageLaser implements Service {
 	{
 		this.log = log;
 		this.config = config;
-		Mat2 x = new Mat2(1400, 100, 0, 0);	// vecteur d'état au départ
-		//Mat2[] P = new 
 		
 	}
 	
