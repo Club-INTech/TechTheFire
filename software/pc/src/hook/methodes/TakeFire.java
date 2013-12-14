c7eefc0db4d06b10bbc909b2173ce3a7f43b1147
PF <troll@yopmail.fr>
2013-12-14 18:27:45 +0100

    Prendre feu

--- software/pc/src/hook/methodes/TakeFire.java
+++ software/pc/src/hook/methodes/TakeFire.java
@@ -1,12 +1,19 @@
 package hook.methodes;
 
+import robot.Robot;
 import hook.Executable;
 
 public class TakeFire implements Executable {
 
-	// TODO
+	private Robot robot;
+	
+	public TakeFire(Robot robot)
+	{
+		this.robot = robot;
+	}
+
 	public void execute() {
-		
+		robot.takefire();
 	}
 
 	
