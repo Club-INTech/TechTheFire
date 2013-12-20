cb571d8541438eec52e675cc64c8ad5ad7f29318
Dede <dede@plopmail.fr>
2013-12-20 19:22:28 +0100

    Changeur d'ID et Gestion de la deuxième pince

--- software/avr/atmega324p/Proto-pince/include/comm.hpp
+++ software/avr/atmega324p/Proto-pince/include/comm.hpp
@@ -21,9 +21,12 @@ class Communication
 //constructeur
 
 	Communication():
-		pince (0,1,1023),
-		orientation (1,1,1023),
-		position (2,1,1023)
+		pinceGauche (0,1,1023),
+		orientationGauche (1,1,1023),
+		positionGauche (2,1,1023),
+		pinceDroite (3,1,1023),
+		orientationDroite (4,1,1023),
+		positionDroite (5,1,1023)
 	{
 		serial_pc::init();
 		serial_pc::change_baudrate (9600);
@@ -40,39 +43,69 @@ class Communication
 		{
 			serial_pc::printfln ( "pince" );
 		}
-		else if ( strcmp ( ordre , "o" ) == 0 )			//o = ouvrir
+		else if ( strcmp ( ordre , "og" ) == 0 )			//o = ouvrir
 		{
-			this->ouvrir ();
+			this->ouvrirGauche ();
 		}
-		else if ( strcmp ( ordre , "f" ) == 0 )			//f = fermer
+		else if ( strcmp ( ordre , "od" ) == 0 )		       
 		{
-			this->fermer ();
+			this->ouvrirDroite ();
 		}
- 		else if ( strcmp ( ordre , "a" ) == 0 )			//a = angle
+		else if ( strcmp ( ordre , "fg" ) == 0 )			//f = fermer
+		{
+			this->fermerGauche ();
+		}
+		else if ( strcmp ( ordre , "fd" ) == 0 )			
+		{
+			this->fermerDroite ();
+		}
+ 		else if ( strcmp ( ordre , "ag" ) == 0 )			//a = angle
 		{
 			serial_pc::printfln ( "angle?" );			
 			int i;
 			serial_pc::read (i);
-			pince.goTo (i);
+			pinceGauche.goTo (i);
 		}
-		else if ( strcmp ( ordre , "b" ) == 0)
+		else if ( strcmp ( ordre , "ad" ) == 0 )		       
 		{
-			this -> bas ();
+			serial_pc::printfln ( "angle?" );			
+			int i;
+			serial_pc::read (i);
+			pinceDroite.goTo (i);
 		}
-		else if ( strcmp (ordre , "m" ) == 0)
+		else if ( strcmp ( ordre , "bg" ) == 0)
+// b = position basse
 		{
-			this -> milieu ();
+			this -> basGauche ();
 		}
-		else if (strcmp (ordre , "h") ==0)
+		else if ( strcmp ( ordre , "bd" ) == 0)
 		{
-       	       		this -> haut ();
+			this -> basDroite ();
 		}
-		else if (strcmp (ordre , "angle") == 0)
+		else if ( strcmp (ordre , "mg" ) == 0)
+// m = milieu 
+		{
+			this -> milieuGauche ();
+		}
+		else if ( strcmp (ordre , "md" ) == 0)
+		{
+			this -> milieuDroite ();
+		}
+		else if (strcmp (ordre , "hg") ==0)
+		{
+       	       		this -> hautGauche ();
+		}
+		else if (strcmp (ordre , "hd") ==0)
+		{
+       	       		this -> hautDroite ();
+		}
+		else if (strcmp (ordre , "angleg") == 0)
+// angle de l'ax 12 de position
 		{
 			serial_pc::printfln ( "angle?" );			
 			int p,o;
 			serial_pc::read (p);
-			position.goTo (p);
+			positionGauche.goTo (p);
 			  if ((p-150) < 0)
 			    {
 			      o = 330-p ;
@@ -81,35 +114,75 @@ class Communication
 			    {
 			      o = p-150 ;
 			    }
-			orientation.goTo (o);
+			orientationGauche.goTo (o);
+			}
+		else if (strcmp (ordre , "angled") == 0)
+		{
+			serial_pc::printfln ( "angle?" );			
+			int p,o;
+			serial_pc::read (p);
+			positionDroite.goTo (p);
+			  if ((p-150) < 0)
+			    {
+			      o = 330-p ;
+			    }
+			  else
+			    {
+			      o = p-150 ;
+			    }
+			orientationDroite.goTo (o);
 			}
 	}
 
 //actions de la pince		
 	
-	void ouvrir ()
+	void ouvrirGauche ()
 	{
-		pince.goTo (60);
+		pinceGauche.goTo (60);
 	}
-	void fermer ()
+	void fermerGauche ()
 	{
-		pince.goTo (150);
+		pinceGauche.goTo (150);
 	}
-	void bas ()
+	void basGauche ()
 	{
-		orientation.goTo(270);
-		position.goTo(60);
+		orientationGauche.goTo(270);
+		positionGauche.goTo(60);
 	}
-	void milieu ()
+	void milieuGauche ()
 	{
-		orientation.goTo(185);
-		position.goTo(150);
+		orientationGauche.goTo(185);
+		positionGauche.goTo(150);
 	}
-	void haut ()
+	void hautGauche ()
 	{
-	  	position.goTo(240);
+	  	positionGauche.goTo(240);
 		_delay_ms(600);
-		orientation.goTo(100);
+		orientationGauche.goTo(100);
+	}
+	void ouvrirDroite ()
+	{
+		pinceDroite.goTo (180);
+	}
+	void fermerDroite ()
+	{
+		pinceDroite.goTo (90);
+	}
+	void basDroite ()
+	{
+		orientationDroite.goTo(270);
+		positionDroite.goTo(60);
+	}
+	void milieuDroite ()
+	{
+		orientationDroite.goTo(185);
+		positionDroite.goTo(150);
+	}
+	void hautDroite ()
+	{
+	  	positionDroite.goTo(240);
+		_delay_ms(600);
+		orientationDroite.goTo(100);
 	}
 	
 	
