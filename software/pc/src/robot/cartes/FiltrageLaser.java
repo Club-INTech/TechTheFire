4150972e5d535920b8ee1f243e3c0e1dedb19e36
besnier <clement.besnier@telecom-sudparis.eu>
2014-01-28 17:08:23 +0100

    Du calcul matriciel, du filtrage, mais il faut savor inverser des matrices, jama trouvé...

--- software/pc/src/robot/cartes/FiltrageLaser.java
+++ software/pc/src/robot/cartes/FiltrageLaser.java
@@ -1,6 +1,7 @@
 package robot.cartes;
 
 import smartMath.Vec2;
+import robot.cartes.Kalman;
 import utils.Log;
 import utils.Read_Ini;
 import container.Service;
@@ -17,7 +18,7 @@ public class FiltrageLaser implements Service {
 	{
 		this.log = log;
 		dt = (double)0.2;
-		double[][] tab_x = {{1400., 100.,0.,0.}};
+		double[][] tab_x = {{1400.}, {100.},{0.},{0.}};
 		Matrn x = new Matrn(tab_x);
 		double[][] tab_p = {{30,0,0,0},{0,30,0,0},{0,0,10,0},{0,0,0,10}};
 		Matrn P = new Matrn(tab_p);
@@ -29,10 +30,10 @@ public class FiltrageLaser implements Service {
 		Matrn R = new Matrn(tab_r);
 		double[][] tab_q = {{Math.pow(dt, 3)/3., 0, Math.pow(dt, 2.)/2., 0},{0, Math.pow(dt, 3.)/3., 0, Math.pow(dt, 2.)/2},{Math.pow(dt, 2.)/2., 0, 4*dt, 0},{0, Math.pow(dt, 2.)/2, 0, 4*dt}};
 		Matrn Q = new Matrn(tab_q);
-		Kalman filtre_kalman = new Kalman(x, P, F, H, R, Q) /* à remplir*/
-		Vec2[] historique = collections.deque(maxlen=3) //à faire
+		Kalman filtre_kalman = new Kalman(x, P, F, H, R, Q); 
+		Vec2[] historique = new Vec2[3];
 		int valeurs_rejetees = 0;
-		acceleration = null;
+		double acceleration = null;
 		
 		
 	}
@@ -61,7 +62,7 @@ public class FiltrageLaser implements Service {
 	public Vec2 vitesse()
 	{
 		Kalman state = filtre_kalman;
-		return Vitesse((int)(state[2], (int)state[3])) ;
+		return new Vitesse((int)(state[2], (int)state[3])) ;
 	}
 
 	// TODO
@@ -69,7 +70,7 @@ public class FiltrageLaser implements Service {
 	{
 		if(this.filtrage_acceleration(point))
 		{
-			last_point = point;
+			Vec2 last_point = point;
 		}
 	}
 	
@@ -86,18 +87,18 @@ public class FiltrageLaser implements Service {
 		Vec2 vitesse_actuelle = pointm0.MinusNewVector(pointm1);
 		Vec2 vitesse_m1 = pointm1.MinusNewVector(pointm2);
 		Vec2 vitesse_m2 = pointm2.MinusNewVector(pointm3);
-		Vec2 acceleration_actuelle = vitesse_actuelle.MinusNewVector(vitessem1);
+		Vec2 acceleration_actuelle = vitesse_actuelle.MinusNewVector(vitesse_m1);
 		Vec2 acceleration_precedente = vitesse_m1.MinusNewVector(vitesse_m2);
 		Vec2 jerk = acceleration_actuelle.MinusNewVector(acceleration_precedente);
 		float produit = acceleration_actuelle.dot(vitesse_m1);
-		if(acceleration_actuelle.SquaredLength()/this.dt**2 >50000 & this.valeurs_rejetees < 3 )
+		if(acceleration_actuelle.SquaredLength()/Math.pow(dt,2) >50000 & this.valeurs_rejetees < 3 )
 		{
 			valeurs_rejetees +=1;
 			return false;
 		}
 		else
 		{
-			valeurs_rejetees = 0;
+			int valeurs_rejetees = 0;
 		    return true;
 		}
 	}
