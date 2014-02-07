8d39da577a4ce3e258cbc01417cf2c8d32b491ad
Dede <dede@plopmail.fr>
2014-02-07 15:49:01 +0100

    modif du C++ total

--- software/avr/atmega324p/Total/include/tot.hpp
+++ software/avr/atmega324p/Total/include/tot.hpp
@@ -15,9 +15,11 @@ class Communication
 	Ax12 pinceGauche; //ax12 de la pince gauche
 	Ax12 orientationGauche;
 	Ax12 positionGauche;
+ Ax12 retourneurGauche;
   Ax12 pinceDroite; //ax12 de la pince droite
   Ax12 orientationDroite;
   Ax12 positionDroite; 
+ Ax12 retourneurDroite ;
   Ax12 rateauG;	
   Ax12 rateauD;						//ax12 du rateau
   Ax12 bac;
@@ -31,12 +33,14 @@ class Communication
 		pinceGauche (0,1,1023),
 		orientationGauche (1,1,1023),
 		positionGauche (2,1,1023),
-		pinceDroite (3,1,1023),
-		orientationDroite (4,1,1023),
-		positionDroite (5,1,1023),
-		rateauG (6,1,1023),
-		rateauD (7,1,1023),
-		bac (8,1,1023)
+		retourneurGauche (3,1,1023),
+		pinceDroite (4,1,1023),
+		orientationDroite (5,1,1023),
+		positionDroite (6,1,1023),
+		retourneurDroite (7,1,1023),
+		rateauG (8,1,1023),
+		rateauD (9,1,1023),
+		bac (10,1,1023)
 
 	{
 		serial_pc::init();
@@ -89,6 +93,16 @@ class Communication
 		{
 			this -> basGauche ();
 		}
+		else if ( strcmp ( ordre , "tg" ) == 0)
+// retourner un feu
+		{
+			this -> retournerGauche ();
+		}
+		else if ( strcmp ( ordre , "rg" ) == 0)
+// remettre en position initiale niveau rotation
+		{
+			this -> retablirGauche ();
+		}
 		else if ( strcmp ( ordre , "bd" ) == 0)
 		{
 			this -> basDroite ();
@@ -110,45 +124,12 @@ class Communication
 		{
        	       		this -> hautDroite ();
 		}
+
 		else if (strcmp (ordre, "test")==0)
 		  {
 		    this -> test ();
 		  }
-
-		else if (strcmp (ordre , "angleg") == 0)
-// angle de l'ax 12 de position
-		{
-			serial_pc::printfln ( "angle?" );			
-			uint16_t p,o;
-			serial_pc::read (p);
-			positionGauche.goTo (p);
-			  if ((p-150) < 0)
-			    {
-			      o = 330-p ;
-			    }
-			  else
-			    {
-			      o = p-150 ;
-			    }
-			orientationGauche.goTo (o);
-			}
-		else if (strcmp (ordre , "angled") == 0)
-		{
-			serial_pc::printfln ( "angle?" );			
-			int p,o;
-			serial_pc::read (p);
-			positionDroite.goTo (p);
-			  if ((p-150) < 0)
-			    {
-			      o = 330-p ;
-			    }
-			  else
-			    {
-			      o = p-150 ;
-			    }
-			orientationDroite.goTo (o);
-			}
-else if ( strcmp ( ordre , "bb" ) == 0 )			//bb = bac bas
+		else if ( strcmp ( ordre , "bb" ) == 0 )			//bb = bac bas
 		{
 			this->bacBas ();
 		}
@@ -194,13 +175,13 @@ else if ( strcmp ( ordre , "bb" ) == 0 )			//bb = bac bas
 	
 	void ouvrirGauche ()
 	{
-		pinceGauche.goTo (60);
+		pinceGauche.goTo (0);
 	}
 	void fermerGauche ()
 	{
 //	  uint16_t positionPrecedente = (pinceGauche.getPosition_0_1023());
 //	  uint16_t positionActuelle = positionPrecedente ;
-	  pinceGauche.goTo(155);
+	  pinceGauche.goTo(65);
 /*	  for(int i=0; i<7;i++)
 	      {
 		_delay_ms(100);
@@ -211,20 +192,31 @@ else if ( strcmp ( ordre , "bb" ) == 0 )			//bb = bac bas
 	}
 	void basGauche ()
 	{
-		orientationGauche.goTo(270);
 		positionGauche.goTo(60);
+		orientationGauche.goTo(210);
 	}
 	void milieuGauche ()
 	{
-		orientationGauche.goTo(185);
 		positionGauche.goTo(150);
+		orientationGauche.goTo(120);
+
 	}
 	void hautGauche ()
 	{
 	  	positionGauche.goTo(240);
-		_delay_ms(600);
-		orientationGauche.goTo(100);
+		_delay_ms(300);
+		orientationGauche.goTo(30);
+		_delay_ms(200);
+		orientationGauche.goTo(120); //cet ajout a pour but de mettre la pince verticale, qu'elle prenne moins de place.
 	}
+void retournerGauche ()
+  {
+    retourneurGauche.goTo (60);
+  }
+  void retablirGauche ()
+  {
+    retourneurGauche.goTo (240);
+  }
 	void ouvrirDroite ()
 	{
 		pinceDroite.goTo (180);
@@ -260,6 +252,14 @@ else if ( strcmp ( ordre , "bb" ) == 0 )			//bb = bac bas
 		_delay_ms(600);
 		orientationDroite.goTo(100);
 	}
+ void retournerDroite ()
+  {
+    retourneurDroite.goTo (60);
+  }
+  void retablirDroite ()
+  {
+    retourneurDroite.goTo (240);
+  }
   void test ()
   {
     positionDroite.goToB(200);
