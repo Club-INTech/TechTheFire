6d482436d7ecd94a564407479233fa70477474d4
besnier <clement.besnier@telecom-sudparis.eu>
2014-03-07 16:54:55 +0100

    Script dépôt feu

--- software/pc/src/scripts/ScriptDeposerFeu.java
+++ software/pc/src/scripts/ScriptDeposerFeu.java
@@ -3,6 +3,7 @@ package scripts;
 import java.util.ArrayList;
 
 import exception.MouvementImpossibleException;
+import exception.SerialException;
 import hook.HookGenerator;
 import pathfinding.Pathfinding;
 import robot.Robot;
@@ -14,7 +15,7 @@ import utils.Read_Ini;
 
 /**
  * Script de dépose de feu
- * @author pf
+ * @author pf, krissprolls
  *
  */
 public class ScriptDeposerFeu extends Script {
@@ -26,21 +27,63 @@ public class ScriptDeposerFeu extends Script {
 
 	@Override
 	public ArrayList<Integer> version(final Robot robot, final Table table) {
-		// TODO
 		ArrayList<Integer> versionList = new ArrayList<Integer>();
+		versionList.add(0);
+		versionList.add(1);
+		versionList.add(2);
+		versionList.add(3);
+		versionList.add(4);
 		return versionList;
 	}
 
 	@Override
 	public Vec2 point_entree(int id) {
-		// TODO Auto-generated method stub
+		if (id==0)
+		{
+			return new Vec2(-1150,233);			
+		}
+		if(id == 1)
+		{
+			return new Vec2(1150,233);
+		}
+		if(id == 2)
+		{
+			return new Vec2(0, 1470);
+		}
+		if(id == 3)
+		{
+			return new Vec2(-391, 739);
+		}
+		if(id == 4)
+		{
+			return new Vec2(391, 739);
+		}
 		return null;
 	}
 
 	@Override
-	public int score(int id_version, Robot robot, Table table) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int score(int id_version, Robot robot, Table table) 
+	{
+		//En effet, quand on pose un feu 
+		try{
+		if(robot.isThereFireGauche() && robot.isThereFireDroite())
+		{
+			return 4;
+		}
+		else if(robot.isThereFireGauche() && robot.isThereFireDroite())
+		{
+			return 2;
+		}
+		else
+		{
+			return 0;
+		}
+		}
+		catch(SerialException e) {
+			e.printStackTrace();
+			return 0;
+		}
+		
 	}
 
 	@Override
@@ -50,16 +93,63 @@ public class ScriptDeposerFeu extends Script {
 	}
 
 	@Override
-	protected void execute(int id_version, Robot robot, Table table) throws MouvementImpossibleException
+	protected void execute(int id_version, Robot robot, Table table) throws MouvementImpossibleException, SerialException
 	{
-		// TODO Auto-generated method stub
+		//Suivant là où onva poser, on doit se positionner différemment
+		if (id_version == 0)
+		{
+			robot.tourner((float)(Math.PI+Math.atan(2/3)));
+		}
+		if(id_version == 1)
+		{
+			robot.tourner((float)(-Math.atan(2/3)));
+		}
+		if(id_version == 2)
+		{
+			robot.tourner((float)(-Math.PI));
+		}
+		if(id_version == 3)
+		{
+			robot.tourner((float)(Math.atan(2/3)));
+		}
+		if(id_version == 4)
+		{
+			robot.tourner((float)(Math.PI-Math.atan(2/3)));
+		}
+		if((couleur.equals("rouge") && robot.isFireRedGauche())|| couleur.equals("jaune")&& ! robot.isFireRedGauche())
+		{
+			robot.poserFeuBonCoteGauche();
+		}
+		else
+		{
+			robot.poserFeuEnRetournantGauche();
+		}
+		//On s'occupe de retourner ou non les pinces gauche et droite, de poser le feu et de remonter
+		if((couleur.equals("rouge") && robot.isFireRedDroite()) || (couleur.equals("jaune")&& ! robot.isFireRedDroite()))
+		{
+			robot.poserFeuBonCoteDroit();
+		}
+		else
+		{
+			robot.poserFeuEnRetournantDroit();
+		}
+		robot.avancer(-50);
 		
 	}
 
 	@Override
-	protected void termine(Robot robot, Table table) {
-		// TODO Auto-generated method stub
-		
+	protected void termine(Robot robot, Table table)
+	{
+		try
+		{
+			robot.lever_pince_droite();
+			robot.fermer_pince_droite();
+			robot.lever_pince_gauche();
+			robot.fermer_pince_gauche();
+		}
+		catch(SerialException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
