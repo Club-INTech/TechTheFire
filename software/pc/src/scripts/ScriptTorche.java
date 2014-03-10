902037354b890bcd1f8675256ff52fa42097fc41
besnier <clement.besnier@telecom-sudparis.eu>
2014-03-10 17:43:30 +0100

    Script feu suite, ça peut marcher, mais ...

--- software/pc/src/scripts/ScriptTorche.java
+++ software/pc/src/scripts/ScriptTorche.java
@@ -96,9 +96,9 @@ public class ScriptTorche extends Script {
 	protected void execute(int id_version, Robot robot, Table table)
 			throws MouvementImpossibleException {
 		if(id_version ==0)
+			//Vec2(-600,900)
 		{
 			robot.tourner((float)Math.PI/2);
-			
 		}
 		else if(id_version ==1)
 			//Vec2(600,900);
@@ -118,45 +118,146 @@ public class ScriptTorche extends Script {
 		else if(id_version ==4)
 			// Vec2(-600, 400);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner((float)Math.PI);
 		}	
 		else if(id_version ==5)
 			// Vec2(600,400);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner((float)Math.PI);
 		}	
 		else if(id_version ==6)
 			// Vec2(-1500,1200);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner((float)Math.PI);
 		}	
 		else if(id_version ==7)
 			// Vec2(1500, 1200);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner(0);
 		}	
 		else if(id_version ==8)
 			// Vec2(-200, 0);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner((float)(3*Math.PI/2));
 		}	
 		else if(id_version ==9)
 			// Vec2(200, 0);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner((float)(3*Math.PI/2));
 		}	
 		else if(id_version ==10)
 			// Vec2(-600, 1400);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner((float)Math.PI);
 		}	
 		else if(id_version ==11)
 			// Vec2(600, 1400);
 		{
-			robot.tourner((float)Math.PI/2);
+			robot.tourner((float)0);			
 		}
-		
-		
+		if(!robot.isTient_feu_gauche())
+		{
+			if(id_version == 2  && id_version == 3 && id_version == 4 && id_version == 5 && id_version == 10 && id_version == 11)
+			{
+				//Pour les feux à pousser
+				try {
+				robot.milieu_pince_gauche();
+				robot.avancer(10);
+				robot.avancer(-10);
+				robot.baisser_pince_gauche();
+				robot.ouvrir_pince_gauche();
+				robot.avancer(10);
+				robot.fermer_pince_gauche();
+				robot.avancer(-10);
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			else if(id_version == 0 && id_version == 1)
+			{
+				//Pour les feux à ramasser dans les torches
+				try {
+				robot.ouvrir_pince_gauche();
+				robot.milieu_pince_gauche();
+				robot.fermer_pince_gauche();
+				robot.lever_pince_gauche();
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else if(id_version == 6 && id_version == 7 && id_version == 8 && id_version ==9)
+			{
+				//Pour les feux à tirer
+				try {
+				robot.milieu_pince_gauche();
+				robot.ouvrir_pince_gauche();
+				robot.avancer(10);
+				robot.fermer_pince_gauche();
+				robot.avancer(-10);
+				robot.ouvrir_pince_gauche();
+				robot.baisser_pince_gauche();
+				robot.avancer(5);
+				robot.fermer_pince_gauche();
+				robot.lever_pince_gauche();
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		else if(robot.isFeu_tenu_droite_rouge())
+		{
+			if(id_version == 2  && id_version == 3 && id_version == 4 && id_version == 5 && id_version == 10 && id_version == 11)
+			{
+				try {
+				robot.milieu_pince_droite();
+				robot.avancer(10);
+				robot.avancer(-10);
+				robot.baisser_pince_droite();
+				robot.ouvrir_pince_droite();
+				robot.avancer(10);
+				robot.fermer_pince_droite();
+				robot.avancer(-10);
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}
+			}
+			else if(id_version == 0 && id_version == 1)
+			{
+				//Pour les feux à ramasser dans les torches
+				try {
+				robot.ouvrir_pince_droite();
+				robot.milieu_pince_droite();
+				robot.fermer_pince_droite();
+				robot.lever_pince_droite();
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else if(id_version == 6 && id_version == 7 && id_version == 8 && id_version ==9)
+			{
+				try {
+				robot.milieu_pince_droite();
+				robot.ouvrir_pince_droite();
+				robot.avancer(10);
+				robot.fermer_pince_droite();
+				robot.avancer(-10);
+				robot.ouvrir_pince_droite();
+				robot.baisser_pince_droite();
+				robot.avancer(5);
+				robot.fermer_pince_droite();
+				robot.lever_pince_droite();
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	@Override
