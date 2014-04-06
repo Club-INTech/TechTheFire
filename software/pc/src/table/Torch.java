6e05ea5291e9aa5f84aebf32f568217f80066163
PF <troll@yopmail.fr>
2014-04-07 01:59:29 +0200

    Simplification des torches

--- software/pc/src/table/Torch.java
+++ software/pc/src/table/Torch.java
@@ -9,16 +9,38 @@ import smartMath.Vec2;
  */
 public class Torch extends Game_Element {
 	
-	private Fire[] fires;
+//	private Fire[] fires;
+	private boolean disparue = false; // True si elle est dégommée
 
-	public Torch(Vec2 position, Fire fire0, Fire fire1, Fire fire2)
+/*	public Torch(Vec2 position, Fire fire0, Fire fire1, Fire fire2)
 	{
 		super(position);
 		fires = new Fire[3];
 		fires[0] = fire0;
 		fires[1] = fire1;
 		fires[2] = fire2;
+	}*/
+
+	public Torch(Vec2 position)
+	{
+		super(position);
 	}
+	
+	public void setDisparue()
+	{
+		disparue = true;
+	}
+	
+	public boolean isDisparue()
+	{
+		return disparue;
+	}
+	
+	public void clone(Torch other)
+	{
+		other.disparue = disparue;
+	}
+	
 /*
 	public void clone(Torch torch)
 	{
