906fb804e6257a35349be0b5cfcc36f5b0a89c8c
PF <troll@yopmail.fr>
2014-04-11 15:53:28 +0200

    Architecture

--- software/pc/src/lanceur.java
+++ software/pc/src/lanceur.java
@@ -54,9 +54,10 @@ public class lanceur
 			robotchrono.majRobotChrono(robotvrai);
 			
 
-			//Talbe
+			//Table
 			Table table = (Table)container.getService("Table");
-			Pathfinding pathfinding = new Pathfinding(table, config, log, 1);
+			Pathfinding pathfinding = new Pathfinding(table, config, log);
+			pathfinding.setPrecision(2);
 			
 			// Etat intial du robot
 			robotvrai.setPosition(new Vec2(1300, 1350));
