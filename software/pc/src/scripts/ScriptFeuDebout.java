87c15c5bd25e048ac3d18ad1639459b2cd824d4e
besnier <clement.besnier@telecom-sudparis.eu>
2014-04-14 00:08:42 +0200

    Feu debout sera certainement inutile mais on sait jamais

---
+++ software/pc/src/scripts/ScriptFeuDebout.java
@@ -0,0 +1,186 @@
+package scripts;
+
+import java.util.ArrayList;
+
+import exception.MouvementImpossibleException;
+import exception.SerialException;
+import hook.HookGenerator;
+import pathfinding.Pathfinding;
+import robot.Cote;
+import robot.Robot;
+import robot.RobotVrai;
+import smartMath.Vec2;
+import table.Table;
+import utils.Log;
+import utils.Read_Ini;
+
+/**
+ * Script de récupération des feux debouts
+ * Pour ceux qui préfèrent séparer les feux dans les torches 
+ * @author  krissprolls
+ *
+ */
+public class ScriptFeuDebout extends Script{
+	
+	
+	public ScriptFeuDebout(HookGenerator hookgenerator, Read_Ini config, Log log, RobotVrai robotvrai)
+	{
+		super(hookgenerator, config, log, robotvrai);
+	}
+
+	public ArrayList<Integer> version(Robot robot, Table table, Pathfinding pathfinding) {
+		// TODO
+		ArrayList<Integer> versionList = new ArrayList<Integer>();
+		//Les feux verticaux
+		//Ajouter une condition pour chaque feu pour savoir s'il est toujours là ?
+		versionList.add(2);
+		versionList.add(3);
+		versionList.add(4);
+		versionList.add(5);
+		versionList.add(10);
+		versionList.add(11);
+		return versionList;
+	}
+
+	public Vec2 point_entree(int id) {
+		//Les coordonnées ont été prises à partir du réglement
+		
+		if(id ==2)
+			return new Vec2(-1100, 700);
+		else if(id ==3)
+			return new Vec2(1100,700);
+		else if(id ==4)
+			return new Vec2(-400, 400);
+		else if(id ==5)
+			return new Vec2(400,400);
+		else if(id ==10)
+			return new Vec2(-400, 1400);
+		else if(id ==11)
+			return new Vec2(400, 1400);
+		else
+			return null;		
+	}
+
+
+	public int score(int id_version, Robot robot, Table table) {
+		return 0;
+	}
+
+
+	public int poids(Robot robot, Table table) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	protected void execute(int id_version, Robot robot, Table table, Pathfinding pathfinding)
+			throws MouvementImpossibleException {
+		if(id_version ==2)
+			//Vec2(-1100, 900);
+		{
+			robot.tourner((float)Math.PI/2);
+		}	
+		else if(id_version ==3)
+			// Vec2(1100,900);
+		{
+			robot.tourner((float)Math.PI/2);
+		}	
+		else if(id_version ==4)
+			// Vec2(-600, 400);
+		{
+			robot.tourner((float)Math.PI);
+		}	
+		else if(id_version ==5)
+			// Vec2(600,400);
+		{
+			robot.tourner((float)Math.PI);
+		}	
+		
+		
+		else if(id_version ==10)
+			// Vec2(-600, 1400);
+		{
+			robot.tourner((float)Math.PI);
+		}	
+		else if(id_version ==11)
+			// Vec2(600, 1400);
+		{
+			robot.tourner((float)0);			
+		}
+		if(!robot.isTient_feu(Cote.GAUCHE))
+		{
+			if(id_version == 2  && id_version == 3 && id_version == 4 && id_version == 5 && id_version == 10 && id_version == 11)
+			{
+				//Pour les feux à pousser
+				
+				try {
+				robot.milieu_pince(Cote.GAUCHE);
+				robot.avancer(10);
+				robot.avancer(-10);
+				robot.baisser_pince(Cote.GAUCHE);
+				robot.ouvrir_pince(Cote.GAUCHE);
+				robot.avancer(10);
+				robot.fermer_pince(Cote.GAUCHE);
+				robot.avancer(-10);
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+			
+			}
+		
+		else if(robot.isFeu_tenu_rouge(Cote.DROIT))
+		{
+			
+			if(id_version == 2  && id_version == 3 && id_version == 4 && id_version == 5 && id_version == 10 && id_version == 11)
+			{
+				try {
+				
+				robot.milieu_pince(Cote.DROIT);
+				robot.avancer(10);
+				robot.avancer(-10);
+				robot.baisser_pince(Cote.DROIT);
+				robot.ouvrir_pince(Cote.DROIT);
+				robot.avancer(10);
+				robot.fermer_pince(Cote.DROIT);
+				robot.avancer(-10);
+				} catch (SerialException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}
+			}
+		}
+	}
+
+	protected void termine(Robot robot, Table table, Pathfinding pathfinding) {
+		try
+		{
+			robot.lever_pince(Cote.DROIT);
+			robot.fermer_pince(Cote.DROIT);
+			robot.lever_pince(Cote.GAUCHE);
+			robot.fermer_pince(Cote.GAUCHE);
+		}
+		catch(SerialException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public float proba_reussite()
+	{
+		// TODO
+		return 1;
+	}
+
+	public String toString()
+	{
+		return "ScriptFeuDebout";
+	}
+	
+	public void maj_config()
+	{
+	}
+
+}
