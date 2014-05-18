ecd90fb7b819e22a431bb556a9adac39bcbc5b56
kayou <pierre.bourre@telecom-sudparis.eu>
2014-05-18 02:25:49 +0200

    Code de la balise qui fonctionne

--- software/pc/src/threads/ThreadLaser.java
+++ software/pc/src/threads/ThreadLaser.java
@@ -20,11 +20,11 @@ class ThreadLaser extends AbstractThread {
 	private FiltrageLaser filtragelaser;
 	private Laser laser;
 	private Table table;
-	
+
 	private int table_x;
 	private int table_y;
 	private float lasers_frequence;
-	
+
 	public ThreadLaser(Laser laser, Table table, FiltrageLaser filtragelaser)
 	{
 		this.filtragelaser = filtragelaser;
@@ -33,7 +33,7 @@ class ThreadLaser extends AbstractThread {
 		maj_config();
 		Thread.currentThread().setPriority(2);
 	}
-	
+
 	@Override
 	public void run()
 	{
@@ -56,21 +56,21 @@ class ThreadLaser extends AbstractThread {
 
 		// Attente de la vitesse stable
 		Sleep.sleep(3000);
-		
+
 		for(Balise balise: laser.balises_ignorees())
 		{
-            log.warning("balise n°" + Integer.toString(balise.id) + " ignorée pendant le match, pas de réponses aux ping", this);
+			log.warning("balise n°" + Integer.toString(balise.id) + " ignorée pendant le match, pas de réponses aux ping", this);
 		}
 
-        // Vérification de la cohérence des données des balises
-        laser.verifier_coherence_balise();
+		// Vérification de la cohérence des données des balises
+		laser.verifier_coherence_balise();
 
-        // Liste des balises prises en compte
-        ArrayList<Balise> balises = laser.balises_actives();
+		// Liste des balises prises en compte
+		ArrayList<Balise> balises = laser.balises_actives();
 
-        while(!ThreadTimer.fin_match)
-        {
-        	long start = System.currentTimeMillis();
+		while(!ThreadTimer.fin_match)
+		{
+			long start = System.currentTimeMillis();
 			if(stop_threads)
 			{
 				log.debug("Stoppage du thread laser", this);
@@ -79,60 +79,60 @@ class ThreadLaser extends AbstractThread {
 
 			for(Balise balise: balises)
 			{
-                // Récupération de la position brute
+				// Récupération de la position brute
 				try {
 					Vec2 p_bruit = laser.position_balise(balise.id); 
-	                // Aucune réponse valable
-	                if(p_bruit == null)
+					// Aucune réponse valable
+					if(p_bruit == null)
 						continue;
-	                
-	                // Mise à jour du modèle de filtrage
-	                filtragelaser.update(p_bruit);
-	
-	                // Récupération des valeurs filtrées
-	                Vec2 p_filtre = filtragelaser.position();
-	
-	                // Vérification si l'obstacle est sur la table 
-	                if(p_filtre.x > -table_x/2 && p_filtre.y > 0 && p_filtre.x < table_x/2 && p_filtre.y < table_y)
-	                    table.deplacer_robot_adverse(balise.id, p_filtre);
-	                
-	                sleep((long)(1./lasers_frequence));
-	                long end = System.currentTimeMillis();
-	                filtragelaser.update_dt((int)(end-start));
+
+					// Mise à jour du modèle de filtrage
+					filtragelaser.update(p_bruit);
+
+					// Récupération des valeurs filtrées
+					Vec2 p_filtre = filtragelaser.position();
+
+					// Vérification si l'obstacle est sur la table 
+					if(p_filtre.x > -table_x/2 && p_filtre.y > 0 && p_filtre.x < table_x/2 && p_filtre.y < table_y)
+						table.deplacer_robot_adverse(balise.id, p_filtre);
+
+					sleep((long)(1./lasers_frequence));
+					long end = System.currentTimeMillis();
+					filtragelaser.update_dt((int)(end-start));
 				}
 				catch(Exception e)
 				{
 					// TODO (désactiver pour le moment car, vu que FiltrageLaser n'est pas fini, y'a des erreurs tout le temps)
-//					e.printStackTrace();
+					//					e.printStackTrace();
 					continue;
 				}
 			}
 
-        }
+		}
 
-        laser.eteindre();
-        log.debug("Fin du thread des lasers", this);
+		laser.eteindre();
+		log.debug("Fin du thread des lasers", this);
 
 	}
-	
+
 	public void maj_config()
 	{
 		try {
-		table_x = Integer.parseInt(config.get("table_x"));
+			table_x = Integer.parseInt(config.get("table_x"));
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
 		try {
-		table_y = Integer.parseInt(config.get("table_y"));
+			table_y = Integer.parseInt(config.get("table_y"));
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
 		try {
-		lasers_frequence = Float.parseFloat(config.get("lasers_frequence"));
+			lasers_frequence = Float.parseFloat(config.get("lasers_frequence"));
 		}
 		catch(Exception e)
 		{
