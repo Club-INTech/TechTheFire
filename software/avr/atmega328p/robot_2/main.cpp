b416c937eb3b4dee1e618186285e46765d29b0cc
kayou <pierre.bourre@telecom-sudparis.eu>
2014-05-30 09:01:32 +0200

    match2

--- software/avr/atmega328p/robot_2/main.cpp
+++ software/avr/atmega328p/robot_2/main.cpp
@@ -10,7 +10,8 @@ int main ()
 {
     Robot2 robot2;
     B5::output();
-    _delay_ms(91000);
+    _delay_ms(10000);
+    //_delay_ms(91000);
     B5::toggle();
     robot2.tirerFilet();
     while (1)
